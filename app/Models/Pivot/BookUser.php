758507dd035dad3115d2b968c64a4aa700beefe6
Pradeepchauhan453 <pradeepchauhan453@gmail.com>
2022-10-06 18:32:24 +0530

    add: Book adding functionality with test

---
+++ app/Models/Pivot/BookUser.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Pivot;
+
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class BookUser extends Pivot
+{
+    public static $statuses = [
+        'WANT_TO_READ' => 'Want to read',
+        'READING' => 'Reading',
+        'READ' => 'Read',
+    ];
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Http\Request;
+use App\Models\Pivot\BookUser;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -41,4 +43,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function books()
+    {
+        return $this->belongsToMany(Book::class)
+        ->using(BookUser::class)
+        ->withPivot('status')
+        ->withTimeStamps();
+    }
 }
